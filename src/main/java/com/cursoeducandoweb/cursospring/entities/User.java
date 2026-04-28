d2e7ecdf6a13d7fddef9bfd7b7e2ef3bc118efd9
Ian Irion <ianpratis9@gmail.com>
2026-04-28 15:36:11 -0300

    adicionando lombok no pom e criando entidade User

---
+++ src/main/java/com/cursoeducandoweb/cursospring/entities/User.java
@@ -0,0 +1,30 @@
+package com.cursoeducandoweb.cursospring.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode(callSuper = false)
+@Table(name = "user")
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+
+}
